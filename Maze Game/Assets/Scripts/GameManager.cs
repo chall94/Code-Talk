aa99b31d851ae410a6773db50be84f84f5b232ae
Christian Hall <30656776+chall94@users.noreply.github.com>
2017-08-25 11:38:52 -0400

    v0.9
    Maze game tutorial from Catlike Coding (http://catlikecoding.com/unity/tutorials/)

---
+++ Maze Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public Maze mazePrefab;
+	private Maze mazeInstance;
+
+	public Player playerPrefab;
+	private Player playerInstance;
+
+	private void Start() {
+		StartCoroutine(BeginGame ());
+	}
+
+	private void Update() {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			RestartGame ();
+		}
+	}
+
+	private IEnumerator BeginGame() {
+		Camera.main.clearFlags = CameraClearFlags.Skybox;
+		Camera.main.rect = new Rect (0f, 0f, 1f, 1f);
+		mazeInstance = Instantiate (mazePrefab) as Maze;
+		yield return StartCoroutine(mazeInstance.Generate ());
+		playerInstance = Instantiate (playerPrefab) as Player;
+		playerInstance.SetLocation(mazeInstance.GetCell(mazeInstance.RandomCoordinates));
+		Camera.main.clearFlags = CameraClearFlags.Depth;
+		Camera.main.rect = new Rect (0f, 0f, 0.25f, 0.5f);
+	}
+
+	private void RestartGame() {
+		StopAllCoroutines ();
+		Destroy (mazeInstance.gameObject);
+		if (playerInstance != null) {
+			Destroy (playerInstance.gameObject);
+		}
+		StartCoroutine(BeginGame ());
+	}
+}
---
+++ Maze Game/Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private MazeCell currentCell;
+
+	private MazeDirection currentDirection;
+
+	public void SetLocation (MazeCell cell) {
+		if (currentCell != null) {
+			currentCell.OnPlayerExited ();
+		}
+		currentCell = cell;
+		transform.localPosition = cell.transform.localPosition;
+		currentCell.OnPlayerEntered ();
+	}
+
+	private void Move (MazeDirection direction) {
+		MazeCellEdge edge = currentCell.GetEdge(direction);
+		if (edge is MazePassage) {
+			SetLocation(edge.otherCell);
+		}
+	}
+
+	private void Rotate (MazeDirection direction) {
+		transform.localRotation = direction.ToRotation();
+		currentDirection = direction;
+	}
+
+	private void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) {
+			Move(currentDirection);
+		}
+		else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) {
+			Move(currentDirection.GetNextClockwise());
+		}
+		else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) {
+			Move(currentDirection.GetOpposite());
+		}
+		else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) {
+			Move(currentDirection.GetNextCounterclockwise());
+		}
+		else if (Input.GetKeyDown(KeyCode.Q)) {
+			Rotate (currentDirection.GetNextCounterclockwise ());
+		}
+		else if (Input.GetKeyDown(KeyCode.E)) {
+			Rotate (currentDirection.GetNextClockwise ());
+		}
+	}
+}
